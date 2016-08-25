f6184d6099c5a3f1f6f532e0c41867ecd46bbed4
Colaco <dlcc@gft.com>
2016-08-25 13:10:52 -0300

    layout

--- app/components/body-list/body.directive.js
+++ app/components/body-list/body.directive.js
@@ -8,8 +8,21 @@
             restrict: 'A',
             replace: true,
             templateUrl: 'app/components/body-list/body.view.html',
-            controller: ['$scope', '$filter', function ($scope, $filter) {
+            controller: ['$scope', '$filter', '$http', function ($scope, $filter, $http) {
+              $http({
+                url: "https://jsonplaceholder.typicode.com/photos",
+                dataType: "json",
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+              })
+              .success(function (data) {
+                  console.log(data);
+              })
+              .error(function (data, status, headers, config) {
 
+              });
             }]
         }
     });
